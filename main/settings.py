7c2816b43cefb94d2475055207c63cbc5df36035
jms429 <jms429@gmail.com>
2020-10-11 19:38:48 -0400

    fixed hosts

--- main/settings.py
+++ main/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'c4dfa=+mct7qyw!q-fej^$l5_fp%jz^g03)97$c)qfagk(xwzq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.203.128']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
