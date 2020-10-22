7354d5be5c3330a9ca2798ea11ed7e1f8c763d92
Jason Frederick <jms429@gmail.com>
2020-10-21 20:23:15 -0400

    reached minimum viable product status

--- employees/admin.py
+++ employees/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Employee, Event
+from .models import Employee, Event, Department
 from django.contrib.auth.models import User, Group
 
 #unregister the group option from admin 
@@ -9,11 +9,14 @@ admin.site.unregister(Group)
 
 class EmployeeAdmin(admin.ModelAdmin):
     list_display = ('full_name', 'is_active')
-    list_filter = ('machine_1', 'machine_2', 'machine_3', 'machine_4','machine_5')
 
 class EventAdmin(admin.ModelAdmin):
     list_display = ('name', 'date', 'event_type')
 
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
 
 admin.site.register(Employee, EmployeeAdmin)
 admin.site.register(Event, EventAdmin)
+admin.site.register(Department, DepartmentAdmin)
